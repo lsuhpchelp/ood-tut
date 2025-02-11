0723ca61444163e573f1cbaf58db9732d6d41fce
QBD4 <fchen14@lsu.edu>
2025-02-11 14:44:11 -0600

    make sure the singularity image is in the correct path

--- add_singularity_kernel.sh
+++ add_singularity_kernel.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-JUPYTER_KERNEL_DIR=~/.local/share/jupyter/kernels/ojk.sigularity
+JUPYTER_KERNEL_DIR=$HOME/.local/share/jupyter/kernels/ojk.singularity
 mkdir -p $JUPYTER_KERNEL_DIR
-cp kernel.json $JUPYTER_KERNEL_DIR/
-
 JUPYTER_KERNEL_FILE="$JUPYTER_KERNEL_DIR/kernel.json"
+
+if [[ -f "kernel.singularity.json" ]]; then
+    cp kernel.singularity.json "$JUPYTER_KERNEL_FILE"
+else
+    echo "kernel.singularity.json not found."
+    echo "Did you git clone the entire repository?"
+    echo "git clone https://github.com/lsuhpchelp/ood-tut.git"
+    echo "Exiting script."
+    exit 1
+fi
+
+sed -i "s|/image_dir/|$(pwd)/|g" $JUPYTER_KERNEL_FILE
 
 if [[ -f "$JUPYTER_KERNEL_FILE" ]]; then
     # Extract content within double quotes from line 8
@@ -12,6 +22,6 @@ if [[ -f "$JUPYTER_KERNEL_FILE" ]]; then
     echo "using the singularity image:"
     sed -n '8s/.*"\(.*\)".*/\1/p' "$JUPYTER_KERNEL_FILE"
 else
-    echo "kernel.json copy failed, make sure kernel.json is in the current directory."
+    echo "kernel.json copy failed, make sure you have write access to $JUPYTER_KERNEL_DIR."
 fi
 
