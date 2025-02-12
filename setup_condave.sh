5d053e72cbc24a38485589ecbf04dcb7760b2c94
QBD4 <fchen14@lsu.edu>
2025-02-12 10:28:19 -0600

    added line to install conda in a non-default path

--- setup_condave.sh
+++ setup_condave.sh
@@ -7,16 +7,20 @@ module load python
 if ! conda info --envs | grep -q "^myenv "; then
     echo "Creating Conda environment 'myenv'..."
     conda create -y --name myenv python=3.10  # Adjust Python version if needed
+    #conda create -y --prefix=$PWD/myenv python=3.10  # Adjust Python version if needed
 else
     echo "Conda environment 'myenv' already exists. Skipping creation."
 fi
 
 # Activate the Conda environment
 source activate myenv
+#conda install ipykernel
+#python -m ipykernel install --user --name myenv --display-name "Python (myenv)"
 
 # Install numpy version 2.0.0 and ipykernel
 echo "Installing numpy 2.0.0 and ipykernel..."
-pip install numpy==2.0.0 ipykernel
+pip install numpy==2.0.0 
+pip install ipykernel #--display-name "Python (My Environment)" 
 
 # Verify installation
 echo "Installed package versions:"
