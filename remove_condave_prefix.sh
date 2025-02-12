6ec31facbf566f0d80352473196f8b3a39ca915b
QBD4 <fchen14@lsu.edu>
2025-02-12 16:45:48 -0600

    easier to change the prefix of conda ve

--- remove_condave_prefix.sh
+++ remove_condave_prefix.sh
@@ -14,15 +14,18 @@ fi
 
 module load python
 
+# here we set the Conda VE prefix to the current dir
+PFX=$(pwd -P)
+
 # Get the name of the currently active Conda environment
 CURRENT_ENV=$(conda info --envs | awk '$1 ~ /^\*/ {print $1}' | sed 's/*//')
 
 # Check if 'myenv' exists before attempting to remove it
 echo "Check if 'myenv' exists..."
-if conda info --envs | grep -q "$PWD/myenv"; then
-    echo "Conda environment '$PWD/myenv' exists..."
+if conda info --envs | grep -q "$PFX/myenv"; then
+    echo "Conda environment '$PFX/myenv' exists..."
     # If currently in 'myenv', deactivate it
-    if [[ "$CONDA_DEFAULT_ENV" == "$PWD/myenv" ]]; then
+    if [[ "$CONDA_DEFAULT_ENV" == "$PFX/myenv" ]]; then
         echo "You are currently in 'myenv'. Deactivating..."
         conda deactivate
         echo "'myenv' has been deactivated."
@@ -34,11 +37,11 @@ if conda info --envs | grep -q "$PWD/myenv"; then
     #fi
 
     # Remove the 'myenv' Conda environment
-    echo "Removing Conda environment '$PWD/myenv'..."
+    echo "Removing Conda environment '$PFX/myenv'..."
     #conda env remove --name myenv
-    conda env remove --prefix "$PWD/myenv"
+    conda env remove --prefix "$PFX/myenv"
     echo "'myenv' has been removed."
 else
-    echo "Conda environment 'myenv' does not exist. No action taken."
+    echo "Conda environment '$PFX/myenv' does not exist. No action taken."
 fi
 
